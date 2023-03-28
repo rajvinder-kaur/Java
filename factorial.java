e5684801674fcc54356335c8375e33f3a7b9d7a2
ace <kaurrajvinder736@gmail.com>
2023-03-28 18:20:16 +0530

    initial commit

---
+++ factorial.java
@@ -0,0 +1,10 @@
+public class factorial {
+  public static void main(String args[]) {
+    int n, f = 1;
+    n = Integer.parseInt(args[0]);
+    for (int i = 1; i <= n; i++) {
+      f = f * i;
+    }
+    System.out.println("factorial=" + f);
+  }
+}
